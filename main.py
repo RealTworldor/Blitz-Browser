76697f10ef7e9c63aafc40e437b8495cd14ae1a5
CarsonTheIdiotGameDev <carsonhall034@gmail.com>
2023-12-22 08:33:09 -0800

    Added the browser part lol
    Made the browser be a browser

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+#This is the blitz browser :)
 import sys
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl('http://google.com'))
+        self.setCentralWidget(self.browser)
         self.showMaximized()
+
+        # navbar
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction('Back', self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
 
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName('Blitz Browser')
 window = MainWindow()
 app.exec_()
-print('Blitz has started sucessfully')
+print('Blitz has started successfully')
