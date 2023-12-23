cfe20c1cc47b6f3846fa3c77387aacfd231ace56
CarsonTheIdiotGameDev <carsonhall034@gmail.com>
2023-12-22 16:26:32 -0800

    Fully rewriting the browsing Engine

--- main.py
+++ main.py
@@ -1,65 +1,10 @@
-#This is the blitz browser :)
+#Base imports
+import os
 import sys
+
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+#WEB ENGINE
 from PyQt5.QtWebEngineWidgets import *
-
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-        self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl('http://search.brave.com'))
-        self.setCentralWidget(self.browser)
-        self.showMaximized()
-
-        # navbar
-        navbar = QToolBar()
-        self.addToolBar(navbar)
-
-        back_btn = QAction('Back', self)
-        back_btn.triggered.connect(self.browser.back)
-        navbar.addAction(back_btn)
-
-        forward_btn = QAction('Foward', self)
-        forward_btn.triggered.connect(self.browser.forward)
-        navbar.addAction(forward_btn)
-
-        home_btn = QAction('Home', self)
-        home_btn.triggered.connect(self.navigate_home)
-        navbar.addAction(home_btn)
-
-        github_page_btn = QAction('Github page', self)
-        github_page_btn.triggered.connect(self.navigate_github_page)
-        navbar.addAction(github_page_btn)
-
-        reload_btn = QAction('Reload', self)
-        reload_btn.triggered.connect(self.browser.reload)
-        navbar.addAction(reload_btn)
-
-        self.url_bar = QLineEdit()
-        self.url_bar.returnPressed.connect(self.navigate_to_url)
-        navbar.addWidget(self.url_bar)
-
-        self.browser.urlChanged.connect(self.update_url)
-
-    def navigate_home(self):
-        self.browser.setUrl(QUrl('http://search.brave.com'))
-
-    def navigate_github_page(self):
-        self.browser.setUrl(QUrl('http://github.com/RealTworldor/Blitz-Browser'))
-
-    def navigate_to_url(self):
-        url = self.url_bar.text()
-        self.browser.setUrl(QUrl(url))
-
-
-    def update_url(self, q):
-        self.url_bar.setText(q.toString())
-
-
-app = QApplication(sys.argv)
-QApplication.setApplicationName('Blitz Browser | 0.1 beta')
-window = MainWindow()
-app.exec_()
-print('Blitz has started successfully')
