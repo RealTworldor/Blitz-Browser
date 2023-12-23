e9c163eff14d8d7ae86e02376a77e6e0ec9b4d09
CarsonTheIdiotGameDev <carsonhall034@gmail.com>
2023-12-22 16:47:49 -0800

    Added the WEB ENGINE and currently working on the Icons

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Base imports
+# Base imports
 import os
 import sys
 
@@ -6,5 +6,44 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-#WEB ENGINE
+# WEB ENGINE
 from PyQt5.QtWebEngineWidgets import *
+
+# MAIN WINDOW
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        # ADD WINDOW ELEMENTS
+        # ADD WINDOW TAB WIDGETS TO DISPLAY WEB TABS
+        self.tabs = QTabWidget()
+        self.tabs.setDocumentMode(True)
+        self.tabs.setTabsClosable(True)
+
+        self.setCentralWidget(self.tabs)
+
+        # ADD NAVIGATION TOOLBAR
+        navtb = QToolBar("Navigation")
+        navtb.setIconSize(QSize(16, 16))
+        self.addToolBar(navtb)
+
+        # ADD BUTTONS TO NAVIGATION TOOLBAR
+        back_btn = QAction(QAction(QIcon))
+
+
+
+        # SHOW MAIN WINDOW
+        self.show()
+
+
+app = QApplication(sys.argv)
+#APPLICATION NAME
+app.setApplicationName("Blitz Web")
+#APPLICATION COMPANY NAME
+app.setOrganizationName("Blitz and Blaze Web")
+#APPLICATION COMPANY DOMAIN
+app.setOrganizationDomain("blitz-and-blaze-web")
+
+window = MainWindow()
+
+app.exec_()
